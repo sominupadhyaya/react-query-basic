59f0c9205ea59e818e22754ec4be783a83d57f44
Somin <sominupadhyaya@gmail.com>
2022-08-05 13:14:20 +0545

    make basic structure of fetching inside of the component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import Characters from './Characters';
 const App = () => {
   return (
     <div className="App">
-      Hello from App
       <Characters/>
    </div>
 
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
 
 const Characters = () =>{
+    
+    const [characters, setCharacters] = useState([])
+    
+    const fetchCharacters = async () =>{
+        const response = await fetch("https://rickandmortyapi.com/api/character")
+        const {results} = await response.json()
+        console.log(results);
+        setCharacters(results)
+    }
+    useEffect(() =>{
+        fetchCharacters()
+    } , [])
+
     return(
         <>
-        
+        {characters.map(({name,id})=>{
+            return (
+            <div key={id}>
+            <h2>
+            {name}
+            </h2>
+            </div>
+            )
+        })}
         </>
     )
 }
